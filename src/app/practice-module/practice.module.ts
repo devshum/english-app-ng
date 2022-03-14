e028ec777823f3af8be55812e7165ea078cae7f8
devshum <57689318+devshum@users.noreply.github.com>
2022-03-14 10:37:14 +0200

    inline-svg

--- src/app/practice-module/practice.module.ts
+++ src/app/practice-module/practice.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { PracticeRoutingModule } from './practice-routing.module';
 import { SharedModuleModule } from './../shared-module/shared-module.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InlineSVGModule } from 'ng-inline-svg-2';
 
 // Components
 import { PracticeComponent } from './components/practice/practice.component';
@@ -16,7 +17,8 @@ import { FormComponent } from './components/form/form.component';
   imports: [
     PracticeRoutingModule,
     SharedModuleModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InlineSVGModule.forRoot({ baseUrl: '/assets/icons/', bypassHttpClientInterceptorChain: true })
   ]
 })
 export class PracticeModule { }
