b88c6fa6b89887cd9732e88659868e8bf55eb67b
devshum <57689318+devshum@users.noreply.github.com>
2022-03-14 10:42:40 +0200

    form-logic

--- src/app/practice-module/components/form/form.component.ts
+++ src/app/practice-module/components/form/form.component.ts
@@ -1,5 +1,5 @@
 // common
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 // interfaces
@@ -10,12 +10,19 @@ import { Verb } from 'src/app/core-module/interfaces/verb.interface';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
+  @ViewChild('pastInput', { static: false }) pastInput: ElementRef;
+  @ViewChild('participleInput', { static: false }) participleInput: ElementRef;
+  @ViewChild('pickBtn', { static: false }) pickBtn: ElementRef;
   @Input() randomVerb: Verb;
   public form: FormGroup;
+  public allowNext = false;
+  private _isPastValid = false;
+  private _isParticipleValid = false;
   //test
-  private validPast = 'forgot';
-  private validParticiple = 'forgotten';
+  private _validPast = 'forgot';
+  private _validParticiple = 'forgotten';
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -29,10 +36,6 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this._initForm();
-  }
-
-  onSubmit(): void {
-    console.log(this.form);
   }
 
   hasError(control: AbstractControl, type: string): boolean {
@@ -49,8 +52,13 @@ export class FormComponent implements OnInit {
   private _checkPast(control: AbstractControl): {[s: string]: boolean} | null {
     const enteredValue = control.value.toLowerCase();
 
-    if(this.validPast !== enteredValue) {
-      return { pastIsCorrect: true };
+    switch (true) {
+      case this._validPast === enteredValue:
+        this._changePastInputState();
+        break;
+
+      case this._validPast !== enteredValue:
+        return { pastIsCorrect: true };
     }
 
     return null;
@@ -59,10 +67,51 @@ export class FormComponent implements OnInit {
   private _checkParticiple(control: AbstractControl): {[s: string]: boolean} | null {
     const enteredValue = control.value.toLowerCase();
 
-    if(this.validParticiple !== enteredValue) {
-      return { participleIsCorrect: true };
+    switch (true) {
+      case this._validParticiple === enteredValue:
+        this._changeParticipleInputFocus();
+        break;
+      case this._validParticiple !== enteredValue:
+        return { participleIsCorrect: true };
     }
 
     return null;
+  }
+
+  private _changeParticipleInputFocus(): void {
+    this._isParticipleValid = true;
+
+    switch (true) {
+      case !this._isPastValid:
+        this._changeFocus(this.pastInput);
+        break;
+
+      case this._isParticipleValid && this._isPastValid:
+        this._allowNext();
+        break;
+    }
+  }
+
+  private _changePastInputState(): void {
+    this._isPastValid = true;
+
+    switch (true) {
+      case !this._isParticipleValid:
+        this._changeFocus(this.participleInput);
+        break;
+
+      case this._isParticipleValid && this._isPastValid:
+        this._allowNext();
+        break;
+    }
+  }
+
+  private _changeFocus(htmlEl: ElementRef): void {
+    htmlEl.nativeElement.focus();
+  }
+
+  private _allowNext(): void {
+    this.allowNext = true;
+    this._changeFocus(this.pickBtn);
   }
 }
