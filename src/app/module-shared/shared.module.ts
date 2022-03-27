3e819a20f92c726034ef64527d87f5fc15686b65
devshum <57689318+devshum@users.noreply.github.com>
2022-03-27 13:22:11 +0300

    search-pipe

--- src/app/module-shared/shared.module.ts
+++ src/app/module-shared/shared.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './components/error/error.component';
+import { SearchVerbkPipe } from '../pipes/search-bookmark.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    ErrorComponent
+    ErrorComponent,
+    SearchVerbkPipe
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   exports: [
     CommonModule,
     RouterModule,
-    ErrorComponent
+    ErrorComponent,
+    SearchVerbkPipe,
+    FormsModule
   ]
 })
 export class SharedModule { }
