20355173cc4cc39ad5644a6a82cfb4a0f59807c5
devshum <57689318+devshum@users.noreply.github.com>
2022-03-29 11:40:35 +0300

    input-search-component

---
+++ src/app/module-shared/components/search/search.component.ts
@@ -0,0 +1,35 @@
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
+import { SearchStorageService } from 'src/app/services/searchStorage.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  @Input() modificator: string;
+  @Input() mode: string;
+
+  @Output() verbValue: EventEmitter<string> = new EventEmitter<string>();
+  @Output() bookmarkValue: EventEmitter<string> = new EventEmitter<string>();
+
+  public searchVerbValue: string;
+  public searchBookmarkValue: string;
+
+  constructor(private _searchStorageService: SearchStorageService) { }
+
+  ngOnInit(): void {
+    this.searchVerbValue = this._searchStorageService.getSearchVerbValue();
+    this.searchBookmarkValue = this._searchStorageService.getSearchBookmarkValue();
+
+    console.log(this.searchBookmarkValue);
+  }
+
+  public inputVerbValue(): void {
+    this.verbValue.emit(this.searchVerbValue);
+  }
+
+  public inputBookmarkValue(): void {
+    this.bookmarkValue.emit(this.searchBookmarkValue);
+  }
+}
