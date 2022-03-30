0e50f8946ec2a298265be2273ad22ab3e330763c
devshum <57689318+devshum@users.noreply.github.com>
2022-03-30 20:42:55 +0300

    perfect-scroll

--- src/app/module-shared/shared.module.ts
+++ src/app/module-shared/shared.module.ts
@@ -1,11 +1,17 @@
+// Modules
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+// Components
 import { ErrorComponent } from './components/error/error.component';
-import { SearchVerbkPipe } from '../pipes/search-bookmark.pipe';
-import { FormsModule } from '@angular/forms';
 import { LoaderComponent } from './components/loader/loader.component';
 import { SearchComponent } from './components/search/search.component';
+
+// Pipes
+import { SearchVerbkPipe } from '../pipes/search-bookmark.pipe';
 
 @NgModule({
   declarations: [
@@ -17,7 +23,8 @@ import { SearchComponent } from './components/search/search.component';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    PerfectScrollbarModule
   ],
   exports: [
     CommonModule,
