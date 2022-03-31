f85009ca8fe3d2407e45ee6756aab29664bde491
devshum <57689318+devshum@users.noreply.github.com>
2022-03-31 12:49:48 +0300

    prepare-routes

---
+++ src/app/module-verbs/components/verbs-search/verbs-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verbs-search',
+  templateUrl: './verbs-search.component.html',
+  styleUrls: ['./verbs-search.component.scss']
+})
+export class VerbsSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/module-verbs/verbs-routing.module.ts
+++ src/app/module-verbs/verbs-routing.module.ts
@@ -1,8 +1,21 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Components
+import { VerbsAllComponent } from './components/verbs-all/verbs-all.component';
+import { VerbsSearchComponent } from './components/verbs-search/verbs-search.component';
 import { VerbsComponent } from './components/verbs/verbs.component';
 
-const routes: Routes = [{ path: '', component: VerbsComponent }];
+const routes: Routes = [
+  { path: '', component: VerbsComponent,
+    children: [
+      { path: '', redirectTo: 'search', pathMatch: 'full' },
+      { path: 'all', component: VerbsAllComponent },
+      { path: 'search', component: VerbsSearchComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
