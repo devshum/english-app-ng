1f1487c6998c18630c8900709228170640f3757c
devshum <57689318+devshum@users.noreply.github.com>
2022-04-28 09:16:02 +0300

    pagination remake

--- src/app/module-verbs/components/table/table.component.ts
+++ src/app/module-verbs/components/table/table.component.ts
@@ -1,28 +1,45 @@
 // Core
 import { Component, Input } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 // Interfaces
 import { newVerb } from './../../../interfaces/newVerb.interface';
+
+// Services
+import { PaginationService } from './../../../services/pagination.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
   @Input() verbs: newVerb[];
+
+  public rowsValue = 1;
 
   public start = 0;
   public middle = 10;
   public end = 20;
+  public currentPage = 1;
+  public verbsPerPage = 20;
 
-  constructor() { }
+  constructor(private _paginationService: PaginationService) { }
 
-  onSliceValues(event: any) {
-    const [start, middle, end] = event;
+  ngOnInit(): void {
+    this.currentPage = this._paginationService.getPage() || this.currentPage;
+    this._calcSlice();
+  }
 
-    this.start = start;
-    this.middle = middle;
-    this.end = end;
+  navigatePage(event: any): void {
+    this.currentPage = event.page + 1;
+    this._paginationService.storePage(this.currentPage);
+    this._calcSlice();
+  }
+
+  private _calcSlice() {
+    this.start = (this.currentPage - 1) * this.verbsPerPage;
+    this.end = this.start + this.verbsPerPage;
+    this.middle = (this.start + this.end) / 2;
   }
 }
--- src/app/module-verbs/verbs.module.ts
+++ src/app/module-verbs/verbs.module.ts
@@ -2,25 +2,30 @@
 import { NgModule } from '@angular/core';
 import { VerbsRoutingModule } from './verbs-routing.module';
 import { SharedModule } from './../module-shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { PaginatorModule } from 'primeng/paginator';
 
 // Components
 import { VerbsComponent } from './components/verbs/verbs.component';
 import { TableComponent } from './components/table/table.component';
 import { TabviewComponent } from './components/tabview/tabview.component';
 import { SearchComponent } from './components/search/search.component';
-import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
   declarations: [
     VerbsComponent,
     TableComponent,
     TabviewComponent,
-    SearchComponent,
-    PaginationComponent
+    SearchComponent
   ],
   imports: [
     SharedModule,
-    VerbsRoutingModule
+    VerbsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    PaginatorModule
   ]
 })
 export class VerbsModule { }
